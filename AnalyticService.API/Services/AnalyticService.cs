eb4eb275c2370516adb649304169f2d4db2d35d5
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-11-05 23:22:59 +0300

    again init

---
+++ AnalyticService.API/Services/AnalyticService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace AnalyticService.API.Services
+{
+    public class DataAnalyticService : DataAnalytic.DataAnalyticBase
+    {
+        public override Task<AnalyticsResponse> GetAnalytics(AnalyticsRequest request, ServerCallContext context)
+        {
+            var response = new AnalyticsResponse
+            {
+                Result = $"Аналитика для ID: {request.Id}",
+                Details = "Подробная информация о запрашиваемой аналитике"
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
