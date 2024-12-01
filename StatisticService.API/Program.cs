13c112e656efd631f7cf0dd79bb9b62aaa0dd8f6
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-12-01 23:02:30 +0300

    fix: добавил (забыл да да) контекст бд в program.cs

--- StatisticService.API/Program.cs
+++ StatisticService.API/Program.cs
@@ -1,10 +1,13 @@
 using StatisticService.BLL.Abstractions;
+using StatisticService.DAL;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<ApplicationContext>();
 
 builder.Services.AddTransient<IStatisticService, StatisticService.BLL.Services.StatisticService>();
 
