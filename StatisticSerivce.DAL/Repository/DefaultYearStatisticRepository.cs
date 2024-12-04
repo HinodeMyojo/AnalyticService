9640bbf4e590569003b4fb2cd2a4350bebf62692
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-12-04 23:19:54 +0300

    feat: добавлен репозиторий для работы с сущностью дефолтной статистики за год

---
+++ StatisticSerivce.DAL/Repository/DefaultYearStatisticRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatisticService.BLL.Abstractions;
+using StatisticService.BLL.Entity;
+
+namespace StatisticService.DAL.Repository
+{
+    public class DefaultYearStatisticRepository : IDefaultYearStatisticRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public DefaultYearStatisticRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task EditDefaultYearStatistic(DefaultYearStatisticEntity entity)
+        {
+            _context.DefaultYearStatisticEntities.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<DefaultYearStatisticEntity?> GetDefaultYearStatistic(int year)
+        {
+            return await _context.DefaultYearStatisticEntities.FirstOrDefaultAsync(x => x.Year == year);
+        }
+
+        public async Task SaveDefaultYearStatistic(DefaultYearStatisticEntity entity)
+        {
+            _context.DefaultYearStatisticEntities.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
