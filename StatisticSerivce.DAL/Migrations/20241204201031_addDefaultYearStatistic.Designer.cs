630d352b78b012707048d5ad8f33313673d80244
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-12-04 23:11:00 +0300

    feat: добавлена миграция для дефолтных значений годовой статистики

--- StatisticSerivce.DAL/Migrations/20241204201031_addDefaultYearStatistic.Designer.cs
+++ StatisticSerivce.DAL/Migrations/20241204201031_addDefaultYearStatistic.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StatisticService.DAL;
@@ -11,9 +12,11 @@ using StatisticService.DAL;
 namespace StatisticService.DAL.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20241204201031_addDefaultYearStatistic")]
+    partial class addDefaultYearStatistic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
